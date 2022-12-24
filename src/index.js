8a107f3c0dd69330f60575d0ab85052b85db5c13
July <110743669+julyivanova@users.noreply.github.com>
2022-12-24 20:34:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementByld("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
