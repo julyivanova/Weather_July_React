ca57c4cfea799d95d5565c04fb908fc6b806b918
July <110743669+julyivanova@users.noreply.github.com>
2022-12-24 19:52:06 +0200

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,7 +12,7 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
-  if (minutes < 10) {
+  if (hours < 10) {
     hours = `0${hours}}`;
   }
   let minutes = props.date.getMinutes();
