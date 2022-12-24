1f7dbb2526d0f1398665c63f20f447cd1075d35c
July <110743669+julyivanova@users.noreply.github.com>
2022-12-24 19:51:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementByld("root"));
+ReactDom.render(<App />, document.getElementByld("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
