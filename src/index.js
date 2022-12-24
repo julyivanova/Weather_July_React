1e4550db095cd6cc3d8a6db0b678ca8128543026
July <110743669+julyivanova@users.noreply.github.com>
2022-12-24 20:31:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App />, document.getElementByld("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
